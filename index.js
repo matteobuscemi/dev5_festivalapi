cbf42509d12273a4c020ca7007bf8d44fa243018
matteobuscemi <43605898+matteobuscemi@users.noreply.github.com>
2021-11-11 20:20:02 +0100

    init: connect server w/ dummy json

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-<<<<<<< Updated upstream
-=======
 const express = require("express");
 const server = express();
 const PORT = 8080;
 
 
 server.get("/", (req, res)=> {
-    const festival = { name: 'Rock Werchter',locatie: "Werchter", startdatum: "30/06/2022" ,einddatum: "3/07/2022" ,capaciteit: "90000" , prijs: "243 euro"};
+    const festival = { 
+        name: 'Rock Werchter',
+        locatie: "Werchter", 
+        startdatum: "30/06/2022",
+        einddatum: "3/07/2022",
+        capaciteit: "90000",
+        prijs: "243 euro"
+    };
 
     res.json(festival)
 
@@ -18,4 +23,3 @@ server.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`);
 
 });
->>>>>>> Stashed changes
