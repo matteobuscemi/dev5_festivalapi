bc875ca9b72fca705ad47aec4db1d3d4610171c5
matteobuscemi <43605898+matteobuscemi@users.noreply.github.com>
2021-11-11 21:10:24 +0100

    feature: start POST-endpoint

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const server = express();
 const PORT = 8080;
 
 
+const pg = require('knex')({
+    client: 'pg',
+    version: '14',
+    searchPath: ['knex', 'public'],
+    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://admin:dev5@localhost:5432/festival_api'
+  });
+
 server.get("/", (req, res)=> {
     const festival = { 
         name: 'Rock Werchter',
@@ -18,6 +25,57 @@ server.get("/", (req, res)=> {
 
 })
 
+server.post("/",async(req,res)=>{
+    const {
+        name: name,
+        locatie: locatie,
+        startdatum: startdatum,
+        einddatum: einddatum,
+        capaciteit: capaciteit,
+        prijs: prijs
+    } = req.body
+    if(name,  locatie, startdatum,  einddatum, capaciteit, prijs){
+  
+          await pg('festival_api').insert({
+            name: name,
+            locatie: locatie,
+            startdatum: startdatum,
+            einddatum: einddatum,
+            capaciteit: capaciteit,
+            prijs: prijs
+        })
+          .then(data => {
+            res.sendStatus(200);
+          })
+  
+    }else{
+      res.sendStatus(400);
+    }
+  });
+
+  async function initialiseTables() {
+    await pg.schema.hasTable('festival_api').then(async (exists) => {
+      if (!exists) {
+        await pg.schema
+          .createTable('festival_api', (table) => {
+            table.increments();
+            table.string('name');
+            table.string('locatie');
+            table.string('startdatum');
+            table.string('einddatum');
+            table.string('capaciteit');
+            table.integer('prijs');
+            table.timestamps(true, true);
+          })
+          .then(async () => {
+  
+          });
+      }else{
+  
+      }
+    });
+  }
+  initialiseTables()
 
 server.listen(PORT, () => {
     console.log(`Server listening at ${PORT}`);
